13d927bd541b7a467102eb69456f0359998601eb
6a6ak <babak.ahari@gmail.com>
2024-09-15 23:19:07 +0200

    range function

---
+++ Basic/Range/range.py
@@ -0,0 +1,12 @@
+'''
+The range function
+range(start,stop,step)
+Creates a sequence of integers starting from start ending before reaching stop increasing by step amounts
+Only the stop argument is mandatory. The defalut value for start is 0
+If two arguments are given they will be start and stop and step will be 1
+Arguments are analogous to slicing arguments
+
+'''
+
+for i in range(1,8,2):
+    print(i)
